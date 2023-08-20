littleGnAl/mirror_platform_frameworks_av

stagefright: set kKeyFrameRate to 1 for thumbnail decoder
For some scenes, such as decoding the thumbnail of 8k@30fps after
recording the video, and then recording the video of 8k@30fps
immediately before it has finished decoding, two codec instances
exist at the same time, and some platforms with weak codec capability
will be overloaded.

thumbnail extracting typically get one frame at a time, so set
kKeyFrameRate to 1 then reduce memory resource consumption.

Bug: 271376224
Change-Id: Ia56bc322145e35a0af3c32ca1b7cffd0c62b6f1a
Signed-off-by: guochuang <guochuang@xiaomi.corp-partner.google.com>

media/libmediaplayerservice/StagefrightMetadataRetriever.cpp
@@ -274,6 +274,11 @@ sp<IMemory> StagefrightMetadataRetriever::getImageInternal(
         }
     }
 
+    // thumbnail extracting typically get one frame at a time,
+    // so set kKeyFrameRate to 1 then reduce memory resource consumption
+    int32_t thumbnailFrameRate = 1;
+    format->setInt32(kKeyFrameRate, thumbnailFrameRate);
+
     MediaCodecList::findMatchingCodecs(
             mime,
             false, /* encoder */
@@ -404,6 +409,11 @@ sp<IMemory> StagefrightMetadataRetriever::getFrameInternal(
         ALOGE("getFrameInternal: convertMetaDataToMessage() failed, unable to extract frame");
         return NULL;
     }
+
+    // thumbnail extracting typically get one frame at a time,
+    // so set kKeyFrameRate to 1 then reduce memory resource consumption
+    int32_t thumbnailFrameRate = 1;
+    format->setInt32(kKeyFrameRate, thumbnailFrameRate);
 
     Vector<AString> matchingCodecs;
     MediaCodecList::findMatchingCodecs(

LineVall/lineage_frameworks_av

Test: run cts -m CtsMediaMiscTestCases

media/libmediaplayerservice/StagefrightMetadataRetriever.cpp
@@ -395,6 +395,11 @@ sp<IMemory> StagefrightMetadataRetriever::getFrameInternal(
         return NULL;
     }
 
+    // thumbnail extracting typically get one frame at a time,
+    // so set kKeyFrameRate to 1 then reduce memory resource consumption
+    int32_t thumbnailFrameRate = 1;
+    trackMeta->setInt32(kKeyFrameRate, thumbnailFrameRate);
+
     bool preferhw = property_get_bool(
             "media.stagefright.thumbnail.prefer_hw_codecs", false);
     uint32_t flags = preferhw ? 0 : MediaCodecList::kPreferSoftwareCodecs;